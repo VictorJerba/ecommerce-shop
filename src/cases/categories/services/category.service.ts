ef0d3afba1e9f6096264f7c2273a40ca27c1d35a
VictorJerba <victorgustavojerba@hotmail.com>
2025-11-23 14:16:14 -0300

    Initial commit

---
+++ src/cases/categories/services/category.service.ts
@@ -0,0 +1,25 @@
+import { api } from "../../../lib/axios";
+import type { CategoryDTO } from "../dtos/category.dto";
+
+const _ENDPOINT = '/categories';
+
+export const CategoryService = {
+
+    async list(): Promise<CategoryDTO[]> {
+        const result = await api.get(_ENDPOINT);
+        return result.data;
+    },
+
+   
+
+    async getById(id: string): Promise<CategoryDTO> {
+        const result = await api.get(`${_ENDPOINT}/${id}`);
+        return result.data;
+    },
+
+    async update(id: string, category: CategoryDTO): Promise<CategoryDTO> {
+        const result = await api.put(`${_ENDPOINT}/${id}`, category);
+        return result.data;
+    },
+
+};
